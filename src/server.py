8b3f2c7ecf6f3eafd3fc76ad9bf0dc7cb5719762
Shawn <shawn.rushefsky@gmail.com>
2024-01-10 11:00:33 -0500

    put gpu name in headers

--- src/server.py
+++ src/server.py
@@ -8,6 +8,7 @@ import io
 import os
 import json
 import requests
+import torch
 from __version__ import __version__
 
 host = os.getenv("HOST", "*")
@@ -16,6 +17,22 @@ salad_machine_id = os.getenv("SALAD_MACHINE_ID", "")
 salad_container_group_id = os.getenv("SALAD_CONTAINER_GROUP_ID", "")
 
 model = load_model()
+
+
+def get_gpu_name():
+    if torch.cuda.is_available():
+        return torch.cuda.get_device_name(0)
+    else:
+        return "CUDA is not available"
+
+
+gpu_name = get_gpu_name()
+
+default_response_headers = {
+    "X-Salad-Machine-ID": salad_machine_id,
+    "X-Salad-Container-Group-ID": salad_container_group_id,
+    "X-GPU-Name": gpu_name,
+}
 
 
 def download_image(url):
@@ -40,11 +57,8 @@ async def get_image_tags(file: UploadFile = File(...)):
     start = time.perf_counter()
     tags = tag_image(model, prepare_image(image))
     stop = time.perf_counter()
-    headers = {
-        "X-Salad-Machine-ID": salad_machine_id,
-        "X-Salad-Container-Group-ID": salad_container_group_id,
-        "X-Inference-Time": f"{stop-start:.4f}",
-    }
+    headers = default_response_headers.copy()
+    headers["X-Inference-Time"] = f"{stop-start:.4f}"
     return Response(content=json.dumps({"tags": tags}), headers=headers)
 
 
@@ -56,12 +70,9 @@ async def get_image_tags_from_url(url: str):
     start = time.perf_counter()
     tags = tag_image(model, prepare_image(image))
     inference_time = time.perf_counter() - start
-    headers = {
-        "X-Salad-Machine-ID": salad_machine_id,
-        "X-Salad-Container-Group-ID": salad_container_group_id,
-        "X-Inference-Time": f"{inference_time:.4f}",
-        "X-Image-Download-Time": f"{image_download_time:.4f}",
-    }
+    headers = default_response_headers.copy()
+    headers["X-Inference-Time"] = f"{inference_time:.4f}"
+    headers["X-Image-Download-Time"] = f"{image_download_time:.4f}"
     return Response(content=json.dumps({"tags": tags}), headers=headers)
 
 
